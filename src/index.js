d75fc021d4b7b7368c391a4d72dcf83e6a009578
MrDomoArigato98 <domdob98@gmail.com>
2025-03-04 12:19:38 +0000

    Todo object class and html templates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,58 @@
 import "./reset.css";
 import "./style.css";
 import gitImg from "./assets/git.png"
- 
-console.log("Hello webpack")
+import {format} from "date-fns"
+
+console.log("Hello webpack");
+
+const date = new Date();
+
+class Todo {
+    constructor(title, description, dueDate, priority, project) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = project
+    }
+
+    editTitle(title) {
+        todoLogger(this.title, title)
+        this.title = title;
+    }
+
+    editDescription(description) {
+        todoLogger(this.description, description)
+        this.description = description;
+    }
+
+    editDueDate(dueDate) {
+        todoLogger(this.dueDate, dueDate)
+        this.dueDate = dueDate;
+    }
+
+    editPriority(priority) {
+        todoLogger(this.priority, priority)
+        this.priority = priority;
+    }
+
+    editProject(project) {
+        todoLogger(this.project, project)
+        this.project = project;
+    }
+}
+
+function todoLogger(old, _new) {
+    console.log("Old value: " + old + ", set to new value: " + _new)
+
+}
+
+class Display {
+
+}
+
+const todo1 = new Todo("MyTitle", "Nothing here", "00.00.00", "1", "default")
+
+console.log(todo1)
+
+todo1.editTitle("New Title")
