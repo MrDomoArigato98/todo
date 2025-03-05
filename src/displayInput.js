d6dbc67e5502752aa0493c4fa21ea83a61845acf
MrDomoArigato98 <domdob98@gmail.com>
2025-03-05 10:20:45 +0000

    Creates module for todo input on screen

---
+++ src/displayInput.js
@@ -0,0 +1,57 @@
+const addTodo = (function () {
+    const addTodoDiv = document.getElementById("add-todo");
+    const todoButtons = addTodoDiv.querySelectorAll("button")
+
+
+    const getInput = () => {
+        const title = document.getElementById("title").value;
+        const description = document.getElementById("description").value;
+        const dueDate = document.getElementById("due-date").value;
+        const prioButton = document.getElementById("priority-btn");
+        let priority;
+        if (isPriority(prioButton)) {
+            priority = "Priority!"
+        } else {
+            priority = 'Not that important'
+        }
+        console.log(title, description, dueDate, priority)
+    }
+
+    const resetInput = () => {
+        document.getElementById("title").value = ""
+        document.getElementById("description").value = ""
+        document.getElementById("due-date").value = ""
+    }
+
+    const isPriority = (button) => {
+        if (button.classList.contains("priority-active")) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    todoButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            if (button.id == 'priority-btn') {
+                button.classList.toggle("priority-active")
+            }
+
+            if (button.id == 'cancel-btn') {
+                console.log("cancel")
+                resetInput()
+
+            }
+
+            if (button.id == 'add-btn') {
+                getInput();
+            }
+
+        })
+    })
+
+    return { getInput, resetInput }
+
+})();
+
+export { addTodo };
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import "./reset.css";
 import "./style.css";
 import gitImg from "./assets/git.png"
 import { format } from "date-fns"
+import { addTodo } from "./displayInput";
 
 const date = new Date();
+const stPattysDay = new Date('2020/03/17');
 
 class Todo {
     constructor(title, description, dueDate, priority) {
@@ -59,20 +61,23 @@ class Display {
 
     }
 
-    
+
 }
 
-const todo1 = new Todo("todo1", "Nothing here", "00.00.00", "1", "default")
-const todo2 = new Todo("todo2", "Nothing here", "00.00.00", "1", "default")
-const todo3 = new Todo("todo3", "Nothing here", "00.00.00", "1", "default")
-const todo4 = new Todo("todo3", "Nothing here", "00.00.00", "1", "default")
-console.log(todo1)
-todo1.editTitle("New Title")
+addTodo.getInput();
+// createTodo.resetInput();
 
-const work = new Project("Test")
-work.addTodo(todo1)
-work.addTodo(todo2)
+// const todo1 = new Todo("todo1", "Nothing here", "00.00.00", "1", "default")
+// const todo2 = new Todo("todo2", "Nothing here", "00.00.00", "1", "default")
+// const todo3 = new Todo("todo3", "Nothing here", "00.00.00", "1", "default")
+// const todo4 = new Todo("todo3", "Nothing here", "00.00.00", "1", "default")
+// console.log(todo1)
+// todo1.editTitle("New Title")
 
-console.log(work)
-work.deleteTodo(todo1)
-console.log(work)
+// const work = new Project("Test")
+// work.addTodo(todo1)
+// work.addTodo(todo2)
+
+// console.log(work)
+// work.deleteTodo(todo1)
+// console.log(work)
