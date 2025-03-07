02bbeb174566c4c60b755420fb481dda0159c4c0
MrDomoArigato98 <domdob98@gmail.com>
2025-03-07 13:17:50 +0000

    lint

--- src/projects.js
+++ src/projects.js
@@ -1,16 +1,15 @@
 class Project {
-    constructor(name) {
-        this.name = name;
-        this.todos = [];
+  constructor(name) {
+    this.name = name;
+    this.todos = [];
+  }
+  push(todo) {
+    this.todos.push(todo);
+  }
 
-    }
-    push(todo) {
-        this.todos.push(todo)
-    }
-
-    deleteTodo(index) {
-        this.todos.splice(index, 1)
-    }
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
 }
 
-export { Project }
+export { Project };
--- src/todo.js
+++ src/todo.js
@@ -1,40 +1,39 @@
 class Todo {
-    constructor(title, description, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
 
-    editTitle(title) {
-        todoLogger(this.title, title)
-        this.title = title;
-    }
+  editTitle(title) {
+    todoLogger(this.title, title);
+    this.title = title;
+  }
 
-    editDescription(description) {
-        todoLogger(this.description, description)
-        this.description = description;
-    }
+  editDescription(description) {
+    todoLogger(this.description, description);
+    this.description = description;
+  }
 
-    editDueDate(dueDate) {
-        todoLogger(this.dueDate, dueDate)
-        this.dueDate = dueDate;
-    }
+  editDueDate(dueDate) {
+    todoLogger(this.dueDate, dueDate);
+    this.dueDate = dueDate;
+  }
 
-    editPriority(priority) {
-        todoLogger(this.priority, priority)
-        this.priority = priority;
-    }
+  editPriority(priority) {
+    todoLogger(this.priority, priority);
+    this.priority = priority;
+  }
 }
 
 function todoLogger(old, _new) {
-    console.log("Old value: " + old + ", set to new value: " + _new)
-
+  console.log("Old value: " + old + ", set to new value: " + _new);
 }
 
 const createTodo = (title, description, dueDate, priority) => {
-    const todo = new Todo(title, description, dueDate, priority)
-    return todo
-}
+  const todo = new Todo(title, description, dueDate, priority);
+  return todo;
+};
 
-export { Todo, createTodo }
+export { Todo, createTodo };
